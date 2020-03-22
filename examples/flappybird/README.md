f8f0f0da7da74cc7be424f6600504a9d26b81359
ducaale <sharaf.13@hotmail.com>
2020-03-22 22:34:39 +0000

    add initial flappybird implementation

---
+++ examples/flappybird/README.md
@@ -0,0 +1,5 @@
+# Flappy Bird
+
+by lordmauve
+via https://github.com/lordmauve/pgzero/tree/master/examples/flappybird
+ported to nyan by Mohamed Daahir
---
+++ examples/flappybird/flappybird.py
@@ -0,0 +1,55 @@
+import nyan
+
+BASE_DIR = 'examples/flappybird/'
+PIPE_GAP = 115
+FLAP_STRENGTH = 12.0
+GRAVITY = 1.6
+
+background = nyan.new_sprite(BASE_DIR + 'background.png')
+pipe_top = nyan.new_sprite(BASE_DIR + 'top.png')
+pipe_bottom = nyan.new_sprite(BASE_DIR + 'bottom.png')
+bird = nyan.new_sprite(BASE_DIR + 'bird0.png')
+
+nyan.screen.width = 300
+nyan.screen.height = 531
+background.size = 75
+
+@nyan.when_program_starts
+def do():
+    bird.vy = FLAP_STRENGTH
+    bird.x = -(nyan.screen.width/3)
+    reset_pipes()
+
+@nyan.repeat_forever
+def move_pipes():
+    pipe_bottom.x -= 5
+    pipe_top.x -= 5
+    if pipe_top.right < nyan.screen.left:
+        reset_pipes()
+
+@nyan.repeat_forever
+async def animate_bird():
+    for image in ['bird0.png', 'bird1.png', 'bird2.png']:
+        bird.image = BASE_DIR + image
+        await nyan.timer(0.1)
+
+@nyan.repeat_forever
+async def do():
+    bird.vy -= GRAVITY
+    bird.y += bird.vy
+    bird.angle = bird.vy * 3
+    if nyan.key_is_pressed('space'):
+        bird.vy = FLAP_STRENGTH
+
+def reset_pipes():
+    pipe_top.left = nyan.screen.right
+    pipe_bottom.left = nyan.screen.right
+
+    bottom_pipe_top = nyan.random_number(
+        -nyan.screen.height/2,
+        (nyan.screen.height/2) - PIPE_GAP
+    )
+    pipe_bottom.top = bottom_pipe_top
+    pipe_top.bottom = bottom_pipe_top + PIPE_GAP
+
+nyan.start_program()
