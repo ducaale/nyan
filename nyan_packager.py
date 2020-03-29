66d246e7466cb7fd92673b19cc7967ce8b4a5323
ducaale <sharaf.13@hotmail.com>
2020-03-29 20:51:50 +0100

    make nyan_packager work when pip installed

--- nyan_packager.py
+++ nyan_packager.py
@@ -7,43 +7,53 @@ import shutil
 
 import PyInstaller.__main__
 
-parser = argparse.ArgumentParser(description='nyan Packager')
-parser.add_argument(
-    'python_file', type=argparse.FileType(), help='nyan program file entry point'
-)
-parser.add_argument(
-    '-i', '--icon', type=argparse.FileType(), help='nyan program icon file'
-)
-args = parser.parse_args()
+def run():
+    parser = argparse.ArgumentParser(description='nyan Packager')
+    parser.add_argument(
+        'python_file', type=argparse.FileType(), help='nyan program file entry point'
+    )
+    parser.add_argument(
+        '-i', '--icon', type=argparse.FileType(), help='nyan program icon file'
+    )
+    parser.add_argument(
+        '-d', '--debug', action='store_true', help='enable console output'
+    )
+    args = parser.parse_args()
 
-dirname = os.path.dirname(args.python_file.name) or "."
-filename = os.path.basename(args.python_file.name)
-name = os.path.splitext(filename)[0]
+    dirname = os.path.dirname(args.python_file.name) or "."
+    filename = os.path.basename(args.python_file.name)
+    name = os.path.splitext(filename)[0]
 
-original_cwd = os.getcwd()
-os.chdir(dirname)
+    original_cwd = os.getcwd()
+    os.chdir(dirname)
 
-assets_src = os.path.join(dirname, 'assets', '*.png')
-assets_dest = 'assets'
-separator = ';' if platform.system() == 'Windows' else ':'
+    assets_src = os.path.join(dirname, 'assets', '*')
+    assets_dest = 'assets'
+    separator = ';' if platform.system() == 'Windows' else ':'
 
-options = [
-    '--clean',
-    f'--name={name}',
-    '--windowed',
-    '--log-level=ERROR',
-    '--distpath=dist',
-    f'--add-data={assets_src}{separator}{assets_dest}',
-    os.path.join(filename),
-]
+    options = [
+        '--clean',
+        f'--name={name}',
+        '--log-level=ERROR',
+        '--distpath=dist',
+        f'--add-data={assets_src}{separator}{assets_dest}',
+        os.path.join(filename),
+    ]
 
-if args.icon:
-    options.append(f'--icon={os.path.abspath(args.icon.name)}')
+    if not args.debug:
+        options.append('--windowed')
+        
+    if args.icon:
+        options.append(f'--icon={args.icon.name}')
 
-PyInstaller.__main__.run(options)
+    PyInstaller.__main__.run(options)
 
-shutil.rmtree('build', ignore_errors=True)
+    build_path = os.path.join(original_cwd, 'build')
+    shutil.rmtree(build_path, ignore_errors=True)
 
-spec_file = os.path.join(original_cwd, f'{name}.spec')
-if os.path.exists(spec_file):
-    os.remove(spec_file)
+    spec_file = os.path.join(original_cwd, f'{name}.spec')
+    if os.path.exists(spec_file):
+        os.remove(spec_file)
+
+if __name__ == "__main__":
+    run()
