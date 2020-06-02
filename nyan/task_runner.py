90286aa0d4883bb1277200296dca9926777d84f4
ducaale <sharaf.13@hotmail.com>
2020-06-02 20:11:22 +0100

    allow keyboard callbacks to be argumentless

--- nyan/task_runner.py
+++ nyan/task_runner.py
@@ -1,16 +1,23 @@
 import asyncio
+import inspect
 
 class TaskRunner():
     def __init__(self):
         self.loop = asyncio.get_event_loop()
         self.running_tasks = set()
 
-    def run(self, task, *args):
-        if (task, *args) in self.running_tasks:
+    def run(self, func, *args):
+        if (func, *args) in self.running_tasks:
             return False
 
-        self.running_tasks.add((task, *args))
-        def on_task_end(f): self.running_tasks.remove((task, *args))
-        future = self.loop.create_task(task(*args))
+        self.running_tasks.add((func, *args))
+        def on_task_end(f): self.running_tasks.remove((func, *args))
+
+        arg_count = len(inspect.signature(func).parameters)
+        if arg_count == 0:
+            future = self.loop.create_task(func())
+        else:
+            future = self.loop.create_task(func(*args))
+
         future.add_done_callback(on_task_end)
         return True
